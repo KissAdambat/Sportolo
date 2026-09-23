59d85d69ac6c258fbd80b3e1459afe374ce07236
KissAdambat <kiss.adi2007@gmail.com>
2026-09-23 08:36:31 +0200

    Add project files.

---
+++ Models/Eredmenyek.cs
@@ -0,0 +1,12 @@
+﻿namespace Sportolo.Models
+{
+    public class Eredmenyek
+    {
+        public int id { get; set; }
+        public string competition { get; set; }
+        public string description { get; set; }
+        public DateTime resultTime { get; set; }
+        public DateTime updateTime { get; set; }
+        public int sportoloId { get; set; }
+    }
+}
