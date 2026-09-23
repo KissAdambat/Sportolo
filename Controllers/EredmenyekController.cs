e3bce0a9ee3a57c270a69407a841e6e6edf879bf
KissAdambat <kiss.adi2007@gmail.com>
2026-09-23 08:48:11 +0200

    gettall works

--- Controllers/EredmenyekController.cs
+++ Controllers/EredmenyekController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
+using Sportolo.Models;
+using System.Data;
 
 namespace Sportolo.Controllers
 {
@@ -7,5 +10,31 @@ namespace Sportolo.Controllers
     [ApiController]
     public class EredmenyekController : ControllerBase
     {
+        private readonly string ConnectionString = "Server=localhost;Database=sportolo13b;User Id=root;Password=";
+        [HttpGet("GETALL")]
+        public List<Eredmenyek> GetAllEredmeny()
+        {
+            List<Eredmenyek> eredmenyek = new();
+            var connector = new MySqlConnection(ConnectionString);
+            connector.Open();
+            string sql = "SELECT * FROM `eredmeny` WHERE 1";
+            var cmd = new MySqlCommand(sql, connector);
+            var dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                var eredmeny = new Eredmenyek
+                {
+                    id = dr.GetInt32("Id"),
+                    competition = dr.GetString("competition"),
+                    description = dr.GetString("description"),
+                    resultTime = dr.GetDateTime("resultTime"),
+                    updateTime = dr.GetDateTime("updateTime"),
+                    sportoloId = dr.GetInt32("sportoloId"),
+                };
+                eredmenyek.Add(eredmeny);
+            }
+            connector.Close();
+            return eredmenyek;
+        }
     }
 }
--- Controllers/SportoloController.cs
+++ Controllers/SportoloController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
+using Sportolo.Models;
 
 namespace Sportolo.Controllers
 {
@@ -7,5 +9,6 @@ namespace Sportolo.Controllers
     [ApiController]
     public class SportoloController : ControllerBase
     {
+       
     }
 }
