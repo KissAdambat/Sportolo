f027f7b7db3ca0336e23ec61819fdc8258960314
KissAdambat <kiss.adi2007@gmail.com>
2026-09-23 08:54:23 +0200

    EredmenyGet

--- Controllers/EredmenyekController.cs
+++ Controllers/EredmenyekController.cs
@@ -36,5 +36,34 @@ namespace Sportolo.Controllers
             connector.Close();
             return eredmenyek;
         }
+
+        [HttpGet("EredmenyGet")]
+        public object EredmenyGet(int id)
+        {
+            var connector = new MySqlConnection(ConnectionString);
+            List<Eredmenyek> eredmenyek = new();
+            connector.Open();
+            var sql = $"SELECT * FROM `eredmeny` WHERE id = @id";
+            var cmd = new MySqlCommand(sql, connector);
+            cmd.Parameters.AddWithValue("@id", id);
+            var dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                var eredmeny = new Eredmenyek
+                {
+                    id = dr.GetInt32("Id"),
+                    competition = dr.GetString("competition"),
+                    description = dr.GetString("description"),
+                    resultTime = dr.GetDateTime("resultTime"),
+                    updateTime = dr.GetDateTime("updateTime"),
+                    sportoloId = dr.GetInt32("sportoloId"),
+                };
+                eredmenyek.Add(eredmeny);
+            }
+            connector.Close();
+            return eredmenyek;
+        }
+
+
     }
 }
