73dc18d53149868e2abd25281364cf3b7c20f1b2
KissAdambat <kiss.adi2007@gmail.com>
2026-09-23 09:49:27 +0200

    SportoloGet

--- Controllers/SportoloController.cs
+++ Controllers/SportoloController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using MySqlConnector;
 using Sportolo.Models;
+using System.Data;
 
 namespace Sportolo.Controllers
 {
@@ -9,6 +10,28 @@ namespace Sportolo.Controllers
     [ApiController]
     public class SportoloController : ControllerBase
     {
-       
+        private readonly string ConnectionString = "Server=localhost;Database=sportolo13b;User Id=root;Password=";
+        [HttpGet("SportoloGet")]
+        public object SportoloGet(int id)
+        {
+            var connector = new MySqlConnection(ConnectionString);
+            List<SportoloCL> sportolok = new();
+            connector.Open();
+            var sql = $"SELECT `name`, `email` FROM `sportolo` WHERE id = @id";
+            var cmd = new MySqlCommand(sql, connector);
+            cmd.Parameters.AddWithValue("@id", id);
+            var dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                var sport = new SportoloCL
+                {
+                    name = dr.GetString("name"),
+                    email = dr.GetString("description"),
+                };
+                sportolok.Add(sport);
+            }
+            connector.Close();
+            return sportolok;
+        }
     }
 }
